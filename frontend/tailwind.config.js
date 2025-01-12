c367f9bae78307474ce80d2af5900ae00091c582
naman katiyar <naman.20233196@mnnit.ac.in>
2025-01-12 16:07:46 +0530

    vite init ,login signup done

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+	  "./index.html",
+	  "./src/**/*.{js,ts,jsx,tsx}", // Include all files in the src folder
+	],
+	theme: {
+	  extend: {},
+	},
+	plugins: [],
+  };
+  
